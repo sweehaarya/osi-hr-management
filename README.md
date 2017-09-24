e5a62edd5745d593592b2a4910ced6a82009f1b6
Roger <rogerchin85@gmail.com>
2017-09-23 18:07:02 -0700

    employee view version 1.0

--- README.md
+++
@@ -1,1 +0,0 @@
-# osi-hr-management
---
+++ index.js
@@ -0,0 +1,46 @@
+// modules
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const pug = require('pug'); // templating engine
+
+// server configurations
+var app = express();
+var http = require('http');
+const server = http.createServer(app); // integrating express with server
+var portNum = 9000; // change to whichever you desire
+const port = process.env.PORT || portNum;
+
+// template configurations
+app.set('views', __dirname + '/templates');
+app.set('view engine', 'pug');
+app.disable('view cache');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// session configurations -- for logins
+app.use(session({
+    secret: 'osi-application', // can change - this is a key used specifically for this application so other application's session doesn't interfere with each other
+    resave: true,
+    saveUninitialized: true
+}));
+
+// static routes
+app.use(['/scripts', '/css', '/images'], express.static('assets')); // by accessing the paths in the array, it will pull files from the assets directory
+
+// routes
+app.get('/', function(req, resp) {
+    resp.render('layout');
+});
+
+// server initialization
+server.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+    }
+
+    console.log('Server is running at ' + port);
+});
