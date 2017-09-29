80861854defccef5009fd784117c8e8ba6cc233a
Roger <rogerchin85@gmail.com>
2017-09-29 13:34:35 -0700

    trying to merge

---
+++ README.md
@@ -0,0 +1,10 @@
+# Osi Maritime Systems Ltd. - Professional Development Plan
+
+## Requirements
+
+ - Node.js
+ - PhantomJS in `PATH`
+ 
+## Run the server
+
+`npm start`
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // modules
 const path = require('path');
 const express = require('express');
@@ -19,11 +20,34 @@ const port = process.env.PORT || portNum;
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'pug');
 app.disable('view cache');
+=======
+// Module imports
+const express = require("express");
+const cookieSession = require('cookie-session')
+const path = require("path");
+const bodyParser = require("body-parser");
+const request = require('request');
+const Xray = require('x-ray');
+const Horseman = require('node-horseman');
+
+
+// Environment variables
+const PORT = process.env.PORT || 3000;
+const DB_URL = '';
+
+// Instanciate the modules
+var app = express();
+
+
+// Middleware config
+app.use("/scripts", express.static('dist/build'));
+>>>>>>> e6e3e4ebf4b61f2038b06858e2a74ddc7db746dc
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+<<<<<<< HEAD
 /*  request('https://55b71886a6adf896951c9f7e77d80c252ed476a1:x@api.bamboohr.com/api/gateway.php/osimaritime/v1/employees/directory', function(err, response, body) {
      parseString(body, function(err, result) {
         console.log(JSON.stringify(result));
@@ -152,3 +176,86 @@ server.listen(port, function(err) {
 
     console.log('Server is running at ' + port);
 });
+=======
+app.use(cookieSession({
+    secret: "osimaritimepdp",
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+
+// Variables for folders path
+var srcFolder = path.resolve(__dirname, "src");
+
+
+
+/* 
+    Routing
+*/
+app.get("/", function (req, resp){
+
+    if(!req.session.userApi) {
+        resp.sendFile(srcFolder + "/logindraft.html");
+    } else {
+        resp.redirect('/home');
+    }
+});
+
+app.post('/login', function(req, resp){
+
+    console.log(req.body);
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // Get Api Key and stores it in client cookies
+    var horseman = new Horseman();
+    horseman
+        .authentication(username, password)
+        .open('https://osimaritime.bamboohr.co.uk/login.php')
+        .waitForSelector('#company-logo>img')
+        .type('input#lname', username)
+        .log("typed username")
+        .type('input#password', password)
+        .log("typed password")
+        .click('.login-actions>button')
+        .log("clicked button")
+        .waitForNextPage({timeout: 20000})
+        .click('#infoLinks>li:nth-child(4)>a')
+        .waitForNextPage({timeout: 20000})
+        .text('.ba-privateContent')
+        .then((key) => {
+            console.log(`Api Key: ${key.trim()}`);
+            req.session.userApi = key.trim();
+            resp.send({redirect: "/home"});
+
+        })
+        .finally(function() {
+            return horseman.close()
+        });
+});
+
+app.get('/logout', (req, resp) => {
+    req.session = null;
+
+    resp.redirect('/');
+});
+
+app.get('/home', function(req, resp) {
+
+    if(req.session.userApi) {
+        console.log(`Cookie Api Key: ${req.session.userApi}`);
+        resp.sendFile(srcFolder + '/home.html');
+    } else {
+        resp.redirect('/');
+    }
+});
+
+
+// Open server
+app.listen(PORT, function(err){
+    if (err) {
+        console.log(err);
+        return false;
+    }
+    console.log(`Server is running on port ${PORT}`);
+});
+
+>>>>>>> e6e3e4ebf4b61f2038b06858e2a74ddc7db746dc
